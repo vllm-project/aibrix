b23fe59bbedbce3b09ddba34bc927f4592938a93
xin.chen <xin.chen@bytedance.com>
2024-08-30 01:25:38 +0800

    update variable name and remove setup script

--- python/aibrix/tests/test_storage.py
+++ python/aibrix/tests/test_storage.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-import aibrix.batchAPI.storage as _storage
+import aibrix.batch.storage as _storage
 
 
 def generate_input_data(num_requests):
@@ -23,14 +23,14 @@ def generate_input_data(num_requests):
 def test_submit_job_input():
     num_request = 100
     generate_input_data(num_request)
-    job_id = _storage.submitJobInput("./one_job_input.json")
+    job_id = _storage.submit_job_input("./one_job_input.json")
     print("succesfully create job: ", job_id)
 
-    input_num = _storage.getJobNumRequest(job_id)
+    input_num = _storage.get_job_num_request(job_id)
     assert input_num == num_request
     print("Total # requeust: ", num_request)
 
-    _storage.deleteJob(job_id)
+    _storage.delete_job(job_id)
     print("remove job id:", job_id)
     os.remove("./one_job_input.json")
 
@@ -38,31 +38,31 @@ def test_submit_job_input():
 def test_read_job_input():
     num_request = 100
     generate_input_data(num_request)
-    job_id = _storage.submitJobInput("./one_job_input.json")
+    job_id = _storage.submit_job_input("./one_job_input.json")
     print("succesfully create job: ", job_id)
 
     # First round, start with an arbitrary index.
     start_idx, num = 50, 10
-    requests = _storage.getJobInputRequests(job_id, start_idx, num)
+    requests = _storage.get_job_input_requests(job_id, start_idx, num)
     for i, req in enumerate(requests):
         custom_id = req["custom_id"]
         assert custom_id == start_idx + i
 
     # Second round, this is a follow-up read.
     start_idx, num = 60, 10
-    requests = _storage.getJobInputRequests(job_id, start_idx, num)
+    requests = _storage.get_job_input_requests(job_id, start_idx, num)
     for i, req in enumerate(requests):
         custom_id = req["custom_id"]
         assert custom_id == start_idx + i
 
     # Thrid round, it reads backward.
     start_idx, num = 30, 20
-    requests = _storage.getJobInputRequests(job_id, start_idx, num)
+    requests = _storage.get_job_input_requests(job_id, start_idx, num)
     for i, req in enumerate(requests):
         custom_id = req["custom_id"]
         assert custom_id == start_idx + i
 
-    _storage.deleteJob(job_id)
+    _storage.delete_job(job_id)
     print("remove job id:", job_id)
     os.remove("./one_job_input.json")
 
@@ -70,19 +70,19 @@ def test_read_job_input():
 def test_job_output():
     num_request = 100
     generate_input_data(num_request)
-    job_id = _storage.submitJobInput("./one_job_input.json")
+    job_id = _storage.submit_job_input("./one_job_input.json")
     print("succesfully create job: ", job_id)
 
     start_idx, num = 50, 10
-    requests = _storage.getJobInputRequests(job_id, start_idx, num)
+    requests = _storage.get_job_input_requests(job_id, start_idx, num)
 
     # Now assuming the output are the same as the input.
-    _storage.putJobResults(job_id, 0, requests)
-    output_reqs = _storage.getJobResults(job_id, 0, len(requests))
+    _storage.put_job_results(job_id, 0, requests)
+    output_reqs = _storage.get_job_results(job_id, 0, len(requests))
     for i, req in enumerate(output_reqs):
         custom_id = req["custom_id"]
         assert custom_id == start_idx + i
 
-    _storage.deleteJob(job_id)
+    _storage.delete_job(job_id)
     print("remove job id:", job_id)
     os.remove("./one_job_input.json")
