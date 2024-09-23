2d9de48e8be2c829627b359dd1f1c7a7d8768558
brosoul <brosoul@126.com>
2024-09-23 21:41:33 +0800

    feat: vllm load & unload detail implement

--- python/aibrix/aibrix/openapi/engine/base.py
+++ python/aibrix/aibrix/openapi/engine/base.py
@@ -12,10 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from dataclasses import dataclass
+from http import HTTPStatus
+from typing import Union
 
 from packaging.version import Version
+
+from aibrix.openapi.protocol import (
+    ErrorResponse,
+    LoadLoraAdapterRequest,
+    UnloadLoraAdapterRequest,
+)
 
 
 @dataclass
@@ -25,23 +33,34 @@ class InferenceEngine(ABC):
     name: str
     version: str
     endpoint: str
+    
+    def _create_error_response(
+        self,
+        message: str,
+        err_type: str = "BadRequestError",
+        status_code: HTTPStatus = HTTPStatus.BAD_REQUEST) -> ErrorResponse:
+        return ErrorResponse(message=message, 
+                             type=err_type, 
+                             code=status_code.value)
 
-    @abstractmethod
-    def load_lora_adapter(self):
-        pass
+    async def load_lora_adapter(self, request: LoadLoraAdapterRequest) -> Union[ErrorResponse, str]:
+        return self._create_error_response(f"Inference engine {self.name} with version {self.version} "
+                                           "not support load lora adapter",
+                                           err_type="NotImplementedError",
+                                           status_code=HTTPStatus.NOT_IMPLEMENTED)
 
-    @abstractmethod
-    def unload_lora_adapter(self):
-        pass
+    async def unload_lora_adapter(self, request: UnloadLoraAdapterRequest) -> Union[ErrorResponse, str]:
+        return self._create_error_response(f"Inference engine {self.name} with version {self.version} "
+                                           "not support unload lora adapter",
+                                           err_type="NotImplementedError",
+                                           status_code=HTTPStatus.NOT_IMPLEMENTED)
 
 
 def get_inference_engine(engine: str, version: str, endpoint: str) -> InferenceEngine:
     if engine.lower() == "vllm":
         # Not support lora dynamic loading & unloading
         if Version(version) < Version("0.6.1"):
-            from aibrix.openapi.engine.vllm import VLLMInferenceEngine
-
-            return VLLMInferenceEngine(engine, version, endpoint)
+            return InferenceEngine(engine, version, endpoint)
         else:
             from aibrix.openapi.engine.vllm import VLLMInferenceEngine
 
