02d4ee2135bc047dec6f0aeeefa2bc0e5f8c8b5b
brosoul <brosoul@126.com>
2024-09-27 15:55:01 +0800

    refact: remove env DOWNLOADER_MODEL_NAME

--- python/aibrix/aibrix/downloader/huggingface.py
+++ python/aibrix/aibrix/downloader/huggingface.py
@@ -31,10 +31,7 @@ def _parse_model_name_from_uri(model_uri: str) -> str:
 class HuggingFaceDownloader(BaseDownloader):
     def __init__(self, model_uri: str, model_name: Optional[str] = None):
         if model_name is None:
-            if envs.DOWNLOADER_MODEL_NAME is not None:
-                model_name = envs.DOWNLOADER_MODEL_NAME
-            else:
-                model_name = _parse_model_name_from_uri(model_uri)
+            model_name = _parse_model_name_from_uri(model_uri)
 
         self.hf_token = envs.DOWNLOADER_HF_TOKEN
         self.hf_endpoint = envs.DOWNLOADER_HF_ENDPOINT
