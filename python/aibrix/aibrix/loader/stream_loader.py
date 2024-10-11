50b9620038acc4bc78bf71b9c84d45f4b014f6c6
brosoul <brosoul@126.com>
2024-10-11 21:38:01 +0800

    load remote safetensors chunck

---
+++ python/aibrix/aibrix/loader/stream_loader.py
@@ -0,0 +1,123 @@
+# Copyright 2024 The Aibrix Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import array
+import concurrent.futures
+import json
+import queue
+import struct
+import threading
+from typing import Generator, List, Tuple, Union
+
+import torch
+
+from aibrix.loader.file import LoadFile
+from aibrix.loader.utils import TensorMeta, split_continue_tensors
+
+
+def get_safetensors_metas(file: LoadFile):
+    LENTH_COUNT = 8
+    length_bytes = file.load_to_bytes(offset=0, count=LENTH_COUNT)
+    length_of_header = struct.unpack("<Q", length_bytes)[0]
+    base_offset = length_of_header + LENTH_COUNT
+    
+    meta_bytes = file.load_to_bytes(offset=LENTH_COUNT, count=length_of_header)
+    tensors_meta = json.loads(meta_bytes.decode("utf-8"))
+    del tensors_meta["__metadata__"]
+    
+    metas: List[TensorMeta] = []
+    for name, tensor_meta in tensors_meta.items():
+        metas.append(TensorMeta(
+            name=name,
+            base_offset=base_offset,
+            dtype=tensor_meta["dtype"],
+            shape=tensor_meta["shape"],
+            data_offsets=tensor_meta["data_offsets"],
+        ))
+    # Ensure tensors chunks could be split continuously
+    sorted_metas = sorted(metas, key=lambda obj: obj.real_offset)
+    return sorted_metas 
+
+
+class StreamLoader():
+    def __init__(self, 
+                 file: LoadFile, 
+                 num_thread: int = 32, 
+                 use_pinmem: bool = False, 
+                 use_direct_io: bool = False):
+        self.file = file
+        self.num_thread = num_thread
+        self.use_pinmem = use_pinmem
+        self.use_direct_io = use_direct_io
+        # TODO assert file type is safetensors
+        self.tensors_metas: List[TensorMeta] = get_safetensors_metas(file)
+
+    def load_safetensors(self, device: Union[torch.device, str] = "cpu"):
+        return dict(self.get_weights_iterator(device=device))
+    
+    
+    def _tensors_reader(self, 
+                       thread_idx, 
+                       barrier, 
+                       device: Union[torch.device, str], 
+                       tensor_metas: Tuple[TensorMeta], 
+                       transfer_out_queue: queue.SimpleQueue[Union[Exception, TensorMeta]]
+                       ):
+        device = torch.device(device)
+        # TODO use stream nonblocking IO
+        for tensor_meta in tensor_metas:
+            
+            tensor_buffer = self.file.load_to_bytes(offset=tensor_meta.real_offset, count=tensor_meta.count)
+            tensor = torch.frombuffer(
+                array.array('b', tensor_buffer),
+                dtype=tensor_meta.dtype
+                ).reshape(tensor_meta.shape).to(device)
+            tensor_meta.set_tensor(tensor)
+            transfer_out_queue.put(tensor_meta)
+    
+    def get_weights_iterator(
+        self,
+        device: Union[torch.device, str] = "cpu"
+    ) -> Generator[Tuple[str, torch.Tensor], None, None]:
+        tensors_per_reader: List[Tuple[TensorMeta]] = split_continue_tensors(self.tensors_metas, self.num_thread)
+        
+        effective_num_readers = len(tensors_per_reader)
+        self._reader_pool = concurrent.futures.ThreadPoolExecutor(
+            max_workers=effective_num_readers,
+            thread_name_prefix="SafetensorsReader",
+        )
+        transfer_out_queue: queue.SimpleQueue[Union[Exception, TensorMeta]] = queue.SimpleQueue() # type: ignore
+        futures: List[concurrent.futures.Future] = []
+
+        barrier = threading.Barrier(effective_num_readers)
+
+        for thread_idx, tensor_metas in enumerate(tensors_per_reader):
+            future = self._reader_pool.submit(
+                self._tensors_reader,
+                thread_idx,
+                barrier,
+                device,
+                tensor_metas,
+                transfer_out_queue,
+            )
+            futures.append(future)
+            
+        try:
+            for _ in range(len(self.tensors_metas)):
+                tensor_meta: TensorMeta = transfer_out_queue.get(timeout=3600)
+                if isinstance(tensor_meta, Exception):
+                    raise tensor_meta
+                yield tensor_meta.name, tensor_meta.tensor
+        except BaseException:
+            raise
---
+++ python/aibrix/aibrix/loader/utils.py
@@ -0,0 +1,111 @@
+from typing import List, Optional, Tuple
+
+import torch
+
+
+def get_dtype(dtype_str: str):
+    # torch.float8 formats require 2.1; we do not support these dtypes on earlier versions
+    _float8_e4m3fn = getattr(torch, "float8_e4m3fn", None)
+    _float8_e5m2 = getattr(torch, "float8_e5m2", None)
+    _TYPES = {
+        "F64": torch.float64,
+        "F32": torch.float32,
+        "F16": torch.float16,
+        "BF16": torch.bfloat16,
+        "I64": torch.int64,
+        # "U64": torch.uint64,
+        "I32": torch.int32,
+        # "U32": torch.uint32,
+        "I16": torch.int16,
+        # "U16": torch.uint16,
+        "I8": torch.int8,
+        "U8": torch.uint8,
+        "BOOL": torch.bool,
+        "F8_E4M3": _float8_e4m3fn,
+        "F8_E5M2": _float8_e5m2,
+    }
+    return _TYPES[dtype_str]
+
+
+class TensorMeta:
+    def __init__(self, name: str, base_offset: int, dtype: str, shape: List[int], data_offsets: List[int]) -> None:
+        self._name = name
+        self._base_offset = base_offset
+        self._dtype = get_dtype(dtype)
+        self._shape = shape
+        self._data_offsets = data_offsets
+        self._tensor = None
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def dtype(self) -> torch.dtype:
+        return self._dtype
+
+    @property
+    def shape(self) -> List[int]:
+        return self._shape
+
+    @property
+    def data_offsets(self) -> List[int]:
+        return self._data_offsets
+
+    @property
+    def real_offset(self) -> List[int]:
+        return self._data_offsets[0] + self._base_offset
+
+    @property
+    def count(self) -> List[int]:
+        return self._data_offsets[1] - self._data_offsets[0]
+
+    @property
+    def tensor(self) -> Optional[torch.Tensor]:
+        return self._tensor
+
+    def set_tensor(self, tensor: torch.Tensor):
+        self._tensor = tensor
+
+    def __str__(self) -> str:
+        return str(
+            {
+                "name": self._name,
+                "dtype": self._dtype,
+                "shape": self._shape,
+                "data_offsets": self._data_offsets,
+            }
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+def split_continue_tensors(tensor_metas: List[TensorMeta], num_readers:int) -> List[Tuple[TensorMeta]]:
+    assert len(tensor_metas) > 0, "tensor_metas should not be empty"
+    assert num_readers > 0, "num_readers should be greater than 0"
+    
+    if len(tensor_metas) <= num_readers:
+        return [tuple(item) for item in tensor_metas]
+    
+    max_offset = tensor_metas[-1].data_offsets[1]
+    avg_size = max_offset // num_readers
+    group = []
+    groups = []
+    current_max_offset = avg_size
+    for tensor_meta in tensor_metas:
+        start, end = tensor_meta.data_offsets
+        while start >= current_max_offset:
+            current_max_offset += avg_size
+        
+        if end <= current_max_offset:
+            group.append(tensor_meta)
+        else:
+            if len(group) != 0:
+                groups.append(tuple(group))
+            group = [tensor_meta]
+            
+            current_max_offset += avg_size
+    if len(group) != 0:
+        groups.append(tuple(group))
+    return groups
