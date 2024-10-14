a2a53466317b8cf1eb3dcd94a601c6974a18fbc9
Varun Gupta <varungup90@gmail.com>
2024-10-14 16:47:25 -0700

    add go routine to add request trace

--- pkg/plugins/gateway/gateway.go
+++ pkg/plugins/gateway/gateway.go
@@ -318,7 +318,9 @@ func (s *Server) HandleResponseBody(ctx context.Context, requestID string, req *
 	}
 
 	defer func() {
-		s.cache.AddRequestTrace(res.Model, res.Usage.PromptTokens, res.Usage.CompletionTokens)
+		go func() {
+			s.cache.AddRequestTrace(res.Model, res.Usage.PromptTokens, res.Usage.CompletionTokens)
+		}()
 	}()
 
 	headers := []*configPb.HeaderValueOption{}
