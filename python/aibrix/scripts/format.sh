8084c3bbac121224d2aecd383c4a78bf1ae93309
brosoul <brosoul@126.com>
2024-11-08 23:38:54 +0800

    add format scripts

---
+++ python/aibrix/scripts/format.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# ruff linter and formatter, adapted from vLLM.
+#
+# Usage:
+#    # Do work and commit your work.
+
+#    # Format files that differ from origin/main.
+#    bash ./scripts/format.sh
+
+# Cause the script to exit if a single command fails
+set -eo pipefail
+
+# this stops git rev-parse from failing if we run this from the .git directory
+ROOT="$(git rev-parse --show-toplevel)/python/aibrix"
+builtin cd "$ROOT" || exit 1
+
+check_command() {
+    if ! command -v "$1" &> /dev/null; then
+        echo "$1 is not installed, please run \`poetry install --no-root --with dev\`"
+        exit 1
+    fi
+}
+
+check_command ruff
+check_command mypy
+
+# Run Ruff
+echo 'vLLM ruff (lint):'
+python -m ruff check . --fix
+echo 'vLLM ruff (format):'
+python -m ruff format .
+echo 'vLLM ruff: Done'
+
+# Run mypy
+echo 'vLLM mypy:'
+python -m mypy .
+echo 'vLLM mypy: Done'
+
+if ! git diff --quiet &>/dev/null; then
+    echo 
+    echo "🔍🔍There are files changed by the format checker or by you that are not added and committed:"
+    git --no-pager diff --name-only
+    echo "🔍🔍Format checker passed, but please add, commit and push all the files above to include changes made by the format checker."
+
+    exit 1
+else
+    echo "✨🎉 Format check passed! Congratulations! 🎉✨"
+fi
