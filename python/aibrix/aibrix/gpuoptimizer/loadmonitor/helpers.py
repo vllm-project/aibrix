009b3410164d94763a396937c3a4cfb680602b96
Jingyuan Zhang <jingyuan.zhang0929@bytedance.com>
2024-11-23 02:37:00 -0800

    Python 3.9 format check

--- python/aibrix/aibrix/gpuoptimizer/loadmonitor/helpers.py
+++ python/aibrix/aibrix/gpuoptimizer/loadmonitor/helpers.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, List, Optional, Tuple, TypeAlias, Union
+from typing import Callable, List, Optional, Tuple, Union
 
 import numpy as np
+from typing_extensions import TypeAlias
 
 DataSignatures: TypeAlias = np.ndarray
 """ndarray of shape(n, 2)"""
