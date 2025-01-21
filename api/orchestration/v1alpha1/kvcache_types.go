d3d568a83f545e2ebd21a7ab1d80d53ee5dc7f60
Jiaxin Shan <seedjeffwan@gmail.com>
2025-01-20 17:13:55 -0800

    Update the kvcache api spec
    Signed-off-by: Jiaxin Shan <seedjeffwan@gmail.com>

--- api/orchestration/v1alpha1/kvcache_types.go
+++ api/orchestration/v1alpha1/kvcache_types.go
@@ -17,25 +17,126 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// ServiceConfig holds all service configuration about KvCache public facing service
+type ServiceConfig struct {
+	// Type defines the type of service (e.g., ClusterIP, NodePort, LoadBalancer).
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:="ClusterIP"
+	Type corev1.ServiceType `json:"type,omitempty"`
+
+	// service port
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=9600
+	Port int32 `json:"port,omitempty"`
+
+	// NodePort specifies the port on each node on which this service is exposed when using NodePort type.
+	// +kubebuilder:validation:Optional
+	NodePort int32 `json:"nodePort,omitempty"`
+}
+
+// MetadataConfig holds the configuration about the kv cache metadata service
+type MetadataConfig struct {
+	Redis RedisConfig `json:"redis,omitempty"`
+	Etcd  EtcdConfig  `json:"etcd,omitempty"`
+}
+
+// RedisConfig provides the configuration fields for deploying Redis.
+type RedisConfig struct {
+	Image     string                      `json:"image"`
+	Replicas  int32                       `json:"replicas"`
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
+	Storage   MetadataStorage             `json:"storage"`
+}
+
+// EtcdConfig provides the configuration fields for deploying etcd.
+type EtcdConfig struct {
+	Image     string                      `json:"image"`
+	Replicas  int32                       `json:"replicas"`
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
+	Storage   MetadataStorage             `json:"storage"`
+}
+
+// MetadataStorage configures the persistent storage used by the metadata service.
+type MetadataStorage struct {
+	Size string `json:"size"`
+}
+
+type CacheSpec struct {
+	// Replicas is the number of kvcache pods to deploy
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default:=3
+	Replicas int `json:"replicas,omitempty"`
+
+	// represent the kvcache's image
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:="aibrix/kvcache:20241120"
+	Image string `json:"image,omitempty"`
+
+	// the policy about pulling image
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:="IfNotPresent"
+	ImagePullPolicy string `json:"imagePullPolicy,omitempty"`
+
+	// shared memory size for kvcach
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=""
+	SharedMemorySize string `json:"sharedMemorySize,omitempty"`
+
+	// kvcache environment configuration
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:={}
+	Env []corev1.EnvVar `json:"env,omitempty"`
+
+	// the memory resources of kvcache container
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:={}
+	Memory string `json:"memory,omitempty"`
+
+	// the cpu resources of kvcache container
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:={}
+	CPU string `json:"cpu,omitempty"`
+}
 
 // KVCacheSpec defines the desired state of KVCache
 type KVCacheSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Replicas is the number of kv cache pods to deploy
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default:=1
+	Replicas int `json:"replicas,omitempty"`
 
-	// Foo is an example field of KVCache. Edit kvcache_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// EtcdReplicas describe the etcd replicas
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=1
+	EtcdReplicas int `json:"etcdReplicas,omitempty"`
+
+	// Metadata configuration for kv cache service
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:={etcd: {image: "", replicas: 1, storage: {size: "10Gi"}}}
+	Metadata MetadataConfig `json:"metadata,omitempty"`
+
+	// kvcache dataplane container configuration
+	// +kubebuilder:validation:Optional
+	//nolint: lll
+	// +kubebuilder:default:={image: "aibrix/kvcache:20241120", imagePullPolicy: "IfNotPresent", socket: "/var/run/aibrix-system/{{.Namespace}}/{{.Name}}", size: ""}
+	Cache CacheSpec `json:"cacheSpec,omitempty"`
+
+	// cache's service
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:={type: "ClusterIP", port: 9600}
+	Service ServiceConfig `json:"service,omitempty"`
 }
 
 // KVCacheStatus defines the observed state of KVCache
 type KVCacheStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Total replicas of current running kv cache instances.
+	ReadyReplicas int32 `json:"current,omitempty"`
+	// Represents the kv cache deployment's current state.
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 //+kubebuilder:object:root=true
