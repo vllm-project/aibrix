66c4aa1e619402c005d9dc7c34d93f1b14950e1a
jolfr <thomas.jack.carroll@gmail.com>
2025-02-26 21:49:52 -0500

    chore: added comments explaining sections
    Signed-off-by: jolfr <thomas.jack.carroll@gmail.com>

--- deployment/terraform/gcp/main.tf
+++ deployment/terraform/gcp/main.tf
@@ -1,14 +1,15 @@
 locals {}
 
 resource "google_container_cluster" "main" {
-  name     = var.cluster_name
+  name = var.cluster_name
+
+  # If cluster region is set, use it, otherwise use default
   location = var.cluster_region != "" ? var.cluster_region : var.default_region
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
+  # Default node pool which immediately gets deleted
   remove_default_node_pool = true
   initial_node_count       = 1
 
+  # Allows cluster to be deleted by terraform
   deletion_protection = false
 }
