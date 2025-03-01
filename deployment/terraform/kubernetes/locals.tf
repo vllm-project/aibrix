0d0022f48ef057204f456ae2448c2f5826f27798
jolfr <thomas.jack.carroll@gmail.com>
2025-02-28 21:34:29 -0500

    fix: updaing keys for map of resources for model
    Signed-off-by: jolfr <thomas.jack.carroll@gmail.com>

--- deployment/terraform/kubernetes/locals.tf
+++ deployment/terraform/kubernetes/locals.tf
@@ -6,5 +6,5 @@ locals {
   aibrix_core_yaml = { for index, resource in provider::kubernetes::manifest_decode_multi(data.http.aibrix_core.response_body) : "${resource.kind}/${resource.metadata.name}" => yamlencode(resource) }
 
   # Model YAML from local repository
-  model_yaml = { for index, resource in provider::kubernetes::manifest_decode_multi(data.http.model.response_body) : "${resource.kind}/${resource.metadata.name}" => yamlencode(resource) }
+  model_yaml = { for index, resource in provider::kubernetes::manifest_decode_multi(data.http.model.response_body) : "${resource.kind}" => yamlencode(resource) }
 }
