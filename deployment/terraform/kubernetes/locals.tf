6475af0cebd1c2144a456e414be0710d300ec658
jolfr <thomas.jack.carroll@gmail.com>
2025-03-03 14:55:55 -0500

    fix: changed to local definition of example model
    Signed-off-by: jolfr <thomas.jack.carroll@gmail.com>

--- deployment/terraform/kubernetes/locals.tf
+++ deployment/terraform/kubernetes/locals.tf
@@ -4,7 +4,4 @@ locals {
 
   # AIBrix core YAML from http retrieval
   aibrix_core_yaml = { for index, resource in provider::kubernetes::manifest_decode_multi(data.http.aibrix_core.response_body) : "${resource.kind}/${resource.metadata.name}" => yamlencode(resource) }
-
-  # Model YAML from local repository
-  model_yaml = { for index, resource in provider::kubernetes::manifest_decode_multi(data.http.model.response_body) : "${resource.kind}" => yamlencode(resource) }
 }
