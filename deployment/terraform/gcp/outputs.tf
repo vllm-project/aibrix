8090a0961908c099d5c5dd7ed83f79fd4b2eda83
jolfr <thomas.jack.carroll@gmail.com>
2025-03-07 23:22:29 -0500

    docs: add Apache License 2.0 copyright headers to Terraform files
    Signed-off-by: jolfr <thomas.jack.carroll@gmail.com>

--- deployment/terraform/gcp/outputs.tf
+++ deployment/terraform/gcp/outputs.tf
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 The Aibrix Team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 output "configure_kubectl_command" {
   description = "Command to run which will allow kubectl access."
   value       = "gcloud container clusters get-credentials ${data.google_container_cluster.main.name} --region ${var.default_region} --project ${var.project_id}"
