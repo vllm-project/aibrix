7c17c9b8a510402e8b47d4ab635012ed9a9a2860
Le Xu <lexuatwork@gmail.com>
2025-03-18 06:08:42 +0800

    update experiment scripts
    Signed-off-by: Le Xu <lexuatwork@gmail.com>

--- benchmarks/autoscaling/run.sh
+++ benchmarks/autoscaling/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+set -x
 #./run.sh workload/workload/25min_up_and_down/25min_up_and_down.jsonl
 
 export KUBECONFIG=~/.kube/config-vke
-export aibrix_repo="/root/aibrix"
+export aibrix_repo="/root/aibrix-local"
 export api_key="sk-kFJ12nKsFVfVmGpj3QzX65s4RbN2xJqWzPYCjYu7wT3BlbLi"
 export kube_context="ccr3aths9g2gqedu8asdg@35122069-kcu0n2lfb7pjdd83330h0"
 
@@ -23,11 +23,16 @@ do
         echo "started experiment at $(date)"
         echo autoscaler: ${autoscaler}
         echo workload: ${workload_path} 
-        echo "The stdout/stderr is being logged in ./output.txt"
-        ./run-test.sh ${workload_path} ${autoscaler} ${aibrix_repo} ${api_key} ${kube_context} &> output-${WORKLOAD_TYPE}.txt 
+        echo "The stdout/stderr is being logged in output-${WORKLOAD_TYPE}.txt"
+        ./run-test.sh ${workload_path} ${autoscaler} ${aibrix_repo} ${api_key} ${kube_context} ${WORKLOAD_TYPE} > output-${WORKLOAD_TYPE}.txt  2>&1
         end_time=$(date +%s)
         echo "Done: Time taken: $((end_time-start_time)) seconds"
         echo "--------------------------------"
         sleep 10
     done
 done
+
+# for WORKLOAD_TYPE in "T_HighSlow_I_HighSlow_O_HighFast"  "T_HighSlow_I_HighSlow_O_HighSlow" "T_HighSlow_I_LowFast_O_HighSlow" "T_HighSlow_I_LowSlow_O_HighSlow"
+# do
+#     python plot-everything.py experiment_results/${WORKLOAD_TYPE}
+# done
