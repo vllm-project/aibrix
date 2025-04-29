f338345f3ac0f6b3d50baa937b9d0d7c700d628b
Venkat Raman <vraman2811@gmail.com>
2025-04-29 21:53:39 +0200

    chore: make lint happy
    Signed-off-by: Venkat Raman <vraman2811@gmail.com>

--- pkg/plugins/gateway/algorithms/vtc/vtc_basic_test.go
+++ pkg/plugins/gateway/algorithms/vtc/vtc_basic_test.go
@@ -24,8 +24,8 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/vllm-project/aibrix/pkg/types"
 	"github.com/vllm-project/aibrix/pkg/metrics"
+	"github.com/vllm-project/aibrix/pkg/types"
 	"github.com/vllm-project/aibrix/pkg/utils"
 )
 
