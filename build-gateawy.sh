0b7346ad1f6bb083fbc17181959d063b3ffd225d
“Gangmuk <gangmuk@gmail.com>
2025-04-29 14:42:20 -0700

    Removed build shell script which was not supposed to be committed
    Signed-off-by: “Gangmuk <gangmuk@gmail.com>

--- build-gateawy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tag=gangmuk-test
-# build
-make docker-build-gateway-plugins
-# tag
-docker tag aibrix/gateway-plugins:nightly aibrix-container-registry-cn-beijing.cr.volces.com/aibrix/gateway-plugins:$tag
-# push
-docker push aibrix-container-registry-cn-beijing.cr.volces.com/aibrix/gateway-plugins:$tag
