ff1403ab156cf759b862ec5bb25dd613a3399ae9
Jingyuan Zhang <jingyuan.zhang0929@bytedance.com>
2025-06-05 13:38:11 -0700

    typo fix
    Signed-off-by: Jingyuan Zhang <jingyuan.zhang0929@bytedance.com>

--- python/aibrix/aibrix/gpu_optimizer/app.py
+++ python/aibrix/aibrix/gpu_optimizer/app.py
@@ -319,7 +319,7 @@ def main(signal, timeout):
                     start_serving_thread(watch_version, deployment, False)
                 except Exception as e:
                     logger.warning(
-                        f"Error on handle existing deployment {deployment.metadata.namepsace}/{deployment.metadata.name}: {e} "
+                        f"Error on handle existing deployment {deployment.metadata.namespace}/{deployment.metadata.name}: {e} "
                     )
         except client.rest.ApiException as ae:
             logger.error(
