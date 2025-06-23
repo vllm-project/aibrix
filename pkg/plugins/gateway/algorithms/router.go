0666cb0e384397fe296a244bc5234a8bd03f4d11
Modi Tamam <modi.tamam@gmail.com>
2025-06-23 09:00:37 +0000

    Added thread safety to all router.go functions
    Signed-off-by: Modi Tamam <modi.tamam@gmail.com>

--- pkg/plugins/gateway/algorithms/router.go
+++ pkg/plugins/gateway/algorithms/router.go
@@ -34,6 +34,8 @@ var routerConstructor = map[types.RoutingAlgorithm]types.RouterProviderRegistrat
 
 // Validate validates if user provided routing routers is supported by gateway
 func Validate(algorithms string) (types.RoutingAlgorithm, bool) {
+	routerMu.RLock()
+	defer routerMu.RUnlock()
 	if _, ok := routerFactory[types.RoutingAlgorithm(algorithms)]; ok {
 		return types.RoutingAlgorithm(algorithms), ok
 	} else {
@@ -44,6 +46,8 @@ func Validate(algorithms string) (types.RoutingAlgorithm, bool) {
 // Select the user provided router provider supported by gateway, no error reported and fallback to random router
 // Call Validate before this function to ensure expected behavior.
 func Select(algorithms types.RoutingAlgorithm) types.RouterProviderFunc {
+	routerMu.RLock()
+	defer routerMu.RUnlock()
 	if provider, ok := routerFactory[algorithms]; ok {
 		return provider
 	} else {
@@ -53,6 +57,8 @@ func Select(algorithms types.RoutingAlgorithm) types.RouterProviderFunc {
 }
 
 func Register(algorithm types.RoutingAlgorithm, constructor types.RouterConstructor) {
+	routerMu.Lock()
+	defer routerMu.Unlock()
 	routerConstructor[algorithm] = func() types.RouterProviderFunc {
 		router, err := constructor()
 		if err != nil {
