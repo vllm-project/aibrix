cf49907652c2796f694d04f9b950215f834e7692
Le Xu <le.xu@bytedance.com>
2025-07-25 14:56:39 -0700

    adding xllm pd driven strategies
    Signed-off-by: Le Xu <le.xu@bytedance.com>

---
+++ pkg/plugins/gateway/algorithms/least_gpu_cache.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2024 The Aibrix Team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package routingalgorithms
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/vllm-project/aibrix/pkg/cache"
+	metrics "github.com/vllm-project/aibrix/pkg/metrics"
+	"github.com/vllm-project/aibrix/pkg/types"
+	v1 "k8s.io/api/core/v1"
+	klog "k8s.io/klog/v2"
+)
+
+const RouterLeastGpuCache types.RoutingAlgorithm = "least-gpu-cache"
+
+func init() {
+	Register(RouterLeastGpuCache, NewLeastGpuCacheRouter)
+}
+
+type leastGpuCacheRouter struct {
+	cache cache.Cache
+}
+
+func NewLeastGpuCacheRouter() (types.Router, error) {
+	c, err := cache.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	return leastGpuCacheRouter{
+		cache: c,
+	}, nil
+}
+
+func (r leastGpuCacheRouter) Route(ctx *types.RoutingContext, readyPodList types.PodList) (string, error) {
+	var targetPod *v1.Pod
+	minGpuCache := math.MaxFloat64
+
+	for _, pod := range readyPodList.All() {
+		// Due to metric refactor (pull/543) to better support lora and multi models,
+		// we change to use PodModelMetrics instead of PodMetrics in some scenarios.
+		// This works but doesn't look very promising, we can revisit this part later.
+		gpuCache, err := r.cache.GetMetricValueByPodModel(pod.Name, pod.Namespace, ctx.Model, metrics.GPUCacheUsagePerc)
+		if err != nil {
+			klog.Error(err)
+			continue
+		}
+		totalCache := gpuCache.GetSimpleValue()
+
+		klog.V(4).Infof("pod: %v, podIP: %v, gpuCache: %v",
+			pod.Name, pod.Status.PodIP, gpuCache.GetSimpleValue())
+
+		if totalCache <= minGpuCache {
+			minGpuCache = totalCache
+			targetPod = pod
+		}
+	}
+
+	// Use fallback if no valid metrics
+	if targetPod == nil {
+		var err error
+		targetPod, err = SelectRandomPodAsFallback(ctx, readyPodList.All(), rand.Intn)
+		if err != nil {
+			return "", err
+		}
+		klog.V(4).Infof("select random targetPod: %s(%s)", targetPod.Name, targetPod.Status.PodIP)
+	} else {
+		klog.V(4).Infof("select targetPod: %s(%s) gpuCache: %v", targetPod.Name, targetPod.Status.PodIP, minGpuCache)
+	}
+
+	if targetPod == nil {
+		return "", fmt.Errorf("no pods to forward request")
+	}
+
+	klog.V(4).Infof("targetPod: %s(%s)", targetPod.Name, targetPod.Status.PodIP)
+	ctx.SetTargetPod(targetPod)
+	return ctx.TargetAddress(), nil
+}
---
+++ pkg/plugins/gateway/algorithms/least_util.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2024 The Aibrix Team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package routingalgorithms
+
+import (
+	"math"
+	"math/rand"
+
+	"github.com/vllm-project/aibrix/pkg/cache"
+	"github.com/vllm-project/aibrix/pkg/metrics"
+	"github.com/vllm-project/aibrix/pkg/types"
+	v1 "k8s.io/api/core/v1"
+	klog "k8s.io/klog/v2"
+)
+
+const RouterUtil types.RoutingAlgorithm = "least-utilization"
+
+func init() {
+	Register(RouterUtil, NewLeastUtilRouter)
+}
+
+type leastUtilRouter struct {
+	cache cache.Cache
+}
+
+func NewLeastUtilRouter() (types.Router, error) {
+	c, err := cache.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	return leastUtilRouter{
+		cache: c,
+	}, nil
+}
+
+func (r leastUtilRouter) Route(ctx *types.RoutingContext, readyPodList types.PodList) (string, error) {
+	var targetPod *v1.Pod
+	minUtilization := math.MaxFloat64 // <= 1 in general
+
+	for _, pod := range readyPodList.All() {
+		utilization, err := r.cache.GetMetricValueByPodModel(pod.Name, pod.Namespace, ctx.Model, metrics.EngineUtilization)
+		if err != nil {
+			klog.Error(err)
+			continue
+		}
+		utilizationValue := utilization.GetSimpleValue()
+		klog.V(4).Infof("pod: %v, podIP: %v, engine utilization: %v", pod.Name, pod.Status.PodIP, utilizationValue)
+
+		if utilizationValue < minUtilization {
+			minUtilization = utilizationValue
+			targetPod = pod
+		}
+	}
+
+	// Use fallback if no valid metrics
+	if targetPod == nil {
+		var err error
+		targetPod, err = SelectRandomPodAsFallback(ctx, readyPodList.All(), rand.Intn)
+		if err != nil {
+			return "", err
+		}
+		klog.V(4).Infof("select random pod: %v, podIP: %v", targetPod.Name, targetPod.Status.PodIP)
+	} else {
+		klog.V(4).Infof("select target pod: %v, podIP: %v, engine utilization: %v", targetPod.Name, targetPod.Status.PodIP, minUtilization)
+	}
+
+	ctx.SetTargetPod(targetPod)
+	return ctx.TargetAddress(), nil
+}
