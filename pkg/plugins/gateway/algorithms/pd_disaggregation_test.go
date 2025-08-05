ea478c8a8102afe01346c8602c565440668be510
Varun Gupta <varungup90@gmail.com>
2025-08-04 21:02:49 -0700

    update tests
    Signed-off-by: Varun Gupta <varungup90@gmail.com>

--- pkg/plugins/gateway/algorithms/pd_disaggregation_test.go
+++ pkg/plugins/gateway/algorithms/pd_disaggregation_test.go
@@ -49,7 +49,8 @@ func TestPDRouter_Route(t *testing.T) {
 		{
 			name: "successful routing with both prefill and decode pods",
 			readyPods: []*v1.Pod{
-				{ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"role-name": "prefill"}, Name: "prefill-1"}, Status: v1.PodStatus{PodIP: "127.0.0.1"}},
+				{ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"role-name": "prefill"}, Name: "prefill-1"}, Status: v1.PodStatus{PodIP: "127.0.0.1",
+					Conditions: []v1.PodCondition{{Type: v1.PodReady, Status: v1.ConditionTrue}}}},
 				{ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"role-name": "decode"}, Name: "decode-1"}, Status: v1.PodStatus{PodIP: "127.0.0.2",
 					Conditions: []v1.PodCondition{{Type: v1.PodReady, Status: v1.ConditionTrue}}}},
 			},
@@ -143,6 +144,12 @@ func TestDoPrefillRequest(t *testing.T) {
 			},
 			Status: v1.PodStatus{
 				PodIP: "127.0.0.1",
+				Conditions: []v1.PodCondition{
+					{
+						Type:   v1.PodReady,
+						Status: v1.ConditionTrue,
+					},
+				},
 			},
 		}
 	}
