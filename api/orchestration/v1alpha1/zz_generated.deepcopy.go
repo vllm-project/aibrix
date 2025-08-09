ab5f19391088701d6bc5d6d814143d46b9c67656
Omer Aplatony <omerap12@gmail.com>
2025-08-09 18:05:57 +0000

    Feat: Support role upgrade sequences in stormservice
    Signed-off-by: Omer Aplatony <omerap12@gmail.com>

--- api/orchestration/v1alpha1/zz_generated.deepcopy.go
+++ api/orchestration/v1alpha1/zz_generated.deepcopy.go
@@ -671,6 +671,11 @@ func (in *RoleSpec) DeepCopyInto(out *RoleSpec) {
 		*out = new(int32)
 		**out = **in
 	}
+	if in.UpgradeOrder != nil {
+		in, out := &in.UpgradeOrder, &out.UpgradeOrder
+		*out = new(int32)
+		**out = **in
+	}
 	if in.PodGroupSize != nil {
 		in, out := &in.PodGroupSize, &out.PodGroupSize
 		*out = new(int32)
