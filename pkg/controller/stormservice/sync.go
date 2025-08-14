e1044229c72fd56253c73d6574e11650b76e5900
cuidajun <dajun.cui@bytedance.com>
2025-08-14 18:53:46 +0800

    [Bug] stormservice's headless service not set ownerRef (#1442)
    * fix: stormservice's headless service not set ownerRef
    
    Signed-off-by: dajun.cui <dajun.cui@bytedance.com>
    
    * fix: patch ut test for service sync
    
    Signed-off-by: dajun.cui <dajun.cui@bytedance.com>
    
    ---------
    
    Signed-off-by: dajun.cui <dajun.cui@bytedance.com>

--- pkg/controller/stormservice/sync.go
+++ pkg/controller/stormservice/sync.go
@@ -76,6 +76,9 @@ func (r *StormServiceReconciler) syncHeadlessService(ctx context.Context, servic
 			Name:      service.Name,
 			Namespace: service.Namespace,
 			Labels:    service.Labels,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(service, orchestrationv1alpha1.SchemeGroupVersion.WithKind(orchestrationv1alpha1.StormServiceKind)),
+			},
 		},
 		Spec: corev1.ServiceSpec{
 			Type:      corev1.ServiceTypeClusterIP,
