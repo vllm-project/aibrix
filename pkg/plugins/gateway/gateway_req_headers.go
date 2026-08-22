13da73f991bf86c26ee7bdebf4eecf848b94535a
henriquejsza <henriquejsza@gmail.com>
2026-08-21 23:33:50 -0300

    fix(gateway): normalize request header keys
    Signed-off-by: henriquejsza <henriquejsza@gmail.com>

--- pkg/plugins/gateway/gateway_req_headers.go
+++ pkg/plugins/gateway/gateway_req_headers.go
@@ -65,11 +65,11 @@ func (s *Server) HandleRequestHeaders(ctx context.Context, requestID string, roo
 		case authorizationKey:
 			reqHeaders[n.Key] = string(n.RawValue)
 		case HeaderExternalFilter:
-			reqHeaders[n.Key] = string(n.RawValue)
+			reqHeaders[HeaderExternalFilter] = string(n.RawValue)
 		case contentTypeKey:
-			reqHeaders[n.Key] = string(n.RawValue)
+			reqHeaders[contentTypeKey] = string(n.RawValue)
 		case HeaderRoutingStrategy:
-			reqHeaders[n.Key] = string(n.RawValue)
+			reqHeaders[HeaderRoutingStrategy] = string(n.RawValue)
 		case HeaderConfigProfile:
 			reqConfigProfile = strings.TrimSpace(string(n.RawValue))
 		case constants.HeaderSessionID:
@@ -77,7 +77,7 @@ func (s *Server) HandleRequestHeaders(ctx context.Context, requestID string, roo
 		case constants.HeaderSessionKey:
 			reqHeaders[constants.HeaderSessionKey] = string(n.RawValue)
 		case HeaderTraceParent: // Preserve the trace context for requests initiated by the gateway plugin. like PD
-			reqHeaders[n.Key] = string(n.RawValue)
+			reqHeaders[HeaderTraceParent] = string(n.RawValue)
 			if !rootSpan.SpanContext().HasTraceID() { // prefers rootSpan traceID over traceparent
 				requestID = GetTraceID(string(n.RawValue), requestID)
 			}
